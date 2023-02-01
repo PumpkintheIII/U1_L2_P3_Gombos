0f185d6df6f58cc60231bf667dcfa81c8de89e7a
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-01 14:48:49 +0000

    Iteration 2

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 int main(void) {
   float num1, num2, num3, num4, num5, numSum, numAvg;
   //initializes functions
+  numSum = 0;
+  numAvg = 0;
   
   printf("Enter a number: "); //asks user for a number
   scanf("%f", &num1);
@@ -30,6 +32,15 @@ int main(void) {
   printf("\nThird number: %f", num3); //prints third number
   printf("\nFourth number: %f", num4); //prints fourth number
   printf("\nFifth number: %f", num5); //prints fifth number
+
+  numSum += num1;
+  numSum += num2;
+  numSum += num3;
+  numSum += num4;
+  numSum += num5;
+  //^gets the sum of all numbers and stores it in numSum
+  
+  printf("\nSum: %f", numSum); //prints numSum
   
   return 0;
 }
